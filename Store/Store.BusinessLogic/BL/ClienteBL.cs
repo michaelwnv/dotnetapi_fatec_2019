4fa711e33e673358c2bfc537e3e7753ef5696feb
minicurso <minicurso@LAB-186>
2019-11-20 22:10:29 -0300

    aula 2

--- Store/Store.BusinessLogic/BL/ClienteBL.cs
+++ Store/Store.BusinessLogic/BL/ClienteBL.cs
@@ -12,8 +12,10 @@ namespace Store.BusinessLogic.BL
 {
     public class ClienteBL : IClienteBL
     {
-        public ClienteBL()
+        private ClienteDao _clienteDao;
+        public ClienteBL() 
         {
+            this._clienteDao = new ClienteDao();
         }
 
         public Cliente Atualizar(Cliente cliente)
@@ -28,7 +30,7 @@ namespace Store.BusinessLogic.BL
 
         public Cliente Inserir(Cliente cliente)
         {
-            throw new NotImplementedException();
+            return this._clienteDao.Insert(cliente);
         }
 
         public List<Cliente> Listar()
@@ -43,6 +45,7 @@ namespace Store.BusinessLogic.BL
 
         public void Dispose()
         {
+            this._clienteDao.Dispose();
         }
     }
 }
